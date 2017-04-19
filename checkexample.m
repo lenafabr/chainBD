47409ae3aa32d6a771c31dbb011d2d5aad1e328e
Lena <lenafabr@gmail.com>
2017-04-19 15:15:15 -0700

    fixed bugs in example1 processing

--- checkexample.m
+++ checkexample.m
@@ -1,14 +1,11 @@
-% look at example snapshot
-data = dlmread('example.dump.out','',0,1);
-beads = data(2:end,:);
-plot3(beads(:,1),beads(:,2),beads(:,3),'.-')
 
 %% read and visualize from file with many snapshots
-data = dlmread('example.dump.out','',0,1);
+data = dlmread('example1.dump.out','',0,1);
 
 nbead = data(1,1);
 nconfig = size(data,1)/(data(1,1)+1)
 
+% look at last snapshot
 beads = data(end-nbead+1:end,:);
 plot3(beads(:,1),beads(:,2),beads(:,3),'.-')
 
@@ -17,13 +14,16 @@ b = nbead+1;
 for cc = 1:nconfig
     beads = data(b*(cc-1)+2:b*cc,:);
     plot3(beads(:,1),beads(:,2),beads(:,3),'.-')
-    view([1,0,0])
+    %view([1,0,0])
+    xlim([-10,10])
+    ylim([-10,10])
+    zlim([-10,10])
     drawnow
     pause(0.1)
 end
 
 %% look at MSD of end bead over time
-data = dlmread('example.out');
+data = dlmread('example1.out');
 
 track = data(:,3:5);
 tracklist = {track};
